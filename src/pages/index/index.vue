bb3c6490abd176e030c28eee996fee624c836e0b
Marx <2443005004@qq.com>
2021-09-12 20:46:55 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    {{ $store.state.title }}
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped>
+view {
+  text-align: center;
+  font-size: 52rpx;
+}
+</style>
